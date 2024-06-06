6390afef7057a1e12f9fc7d16542ef696cd0bb77
Akanksha Dubey <67544813+akanksha-web@users.noreply.github.com>
2024-06-06 16:26:48 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -4,7 +4,15 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   // Implement the user schema fields:
   // name as String and
+  name: {
+    type: String,
+  },
+
+  email: {
+    type:String
+  }
   // email as a String,
 });
 
 // Create and export the User model
+module.exports = mongoose.model("users", userSchema);
