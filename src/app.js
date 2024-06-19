a203279a4f3287f52d7e1d9035f46059f54a8095
Akanksha Dubey <67544813+akanksha-web@users.noreply.github.com>
2024-06-19 13:37:45 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const connectDB = require("./index")
+
 const app = express();
 
-const router = require('../controllers/userControllers');
+const userRoutes = require('../controllers/userControllers');
 
 //Router Middlewares
 app.use(express.json());
 
-app.use('/api', router);
+app.use('/api', userRoutes);
+app.use((err,req,res,next) =>{
+    console.error(err.stack);
+    res.status(500).json({message:"Internal server error"});
+});
 
 module.exports = app;
+
