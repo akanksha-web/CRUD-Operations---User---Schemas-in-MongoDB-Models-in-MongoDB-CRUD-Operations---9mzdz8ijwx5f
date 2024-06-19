f499002881f3780e2a20763c476c7a7d91f8a975
akanksha-web <dubeyakanksha702@gmail.com>
2024-06-19 14:34:40 +0530

    Merge branch 'master' of https://github.com/akanksha-web/CRUD-Operations---User---Schemas-in-MongoDB-Models-in-MongoDB-CRUD-Operations---9mzdz8ijwx5f

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -8,8 +8,21 @@ const router = express.Router();
 router.post('/users', async (req, res) => {
  // Implement user creation logic here
   // 1. Extract user data from the request body (req.body)
+  const userID = req.body.userID;
    console.log("body", req.body);
   // 2. Create a new user using User.create()
+  try {
+    const users = await User.create({
+      id:req.body.userID,
+      name:req.body.name,
+      email:req.body.email
+  })
+  console.log("creating users", users);
+  res.status(201).json({message:"created user", users,userID});
+  } catch (error) {
+    console.log("error", error);
+       res.status(500).json({message:"Internal server error"});
+  }
   // 3. Handle success: Respond with a 201 status code and the created user
   // 4. Handle errors: Respond with appropriate error messages and status codes
 });
@@ -40,6 +53,7 @@ router.patch('/users/:id', async (req, res) => {
   // 1. Extract the user ID from the request parameters (req.params.id)
    const userId = req.params.id;
   // 2. Extract updated user data from the request body (req.body)
+
    const updatedData = {
     name:req.body.name,
     email:req.body.email
